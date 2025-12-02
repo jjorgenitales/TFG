d76c2b7037f7a38d77a30cddf39595d9646a1123
jjorgenitales <jjanacletocarvajal@gmail.com>
2025-12-02 20:36:58 +0100

    Carpeta templates añadido junto a base.html

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    idUsuario = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    clave = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    pais = models.CharField(max_length=50)
+    cp = models.CharField(max_length=10)
+    tipo = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    peso = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return self.nombre
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Usuario
+
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        correo = request.POST.get('correo')
+        clave = request.POST.get('clave')
+        try:
+            usuario = Usuario.objects.get(correo=correo, clave=clave)
+            # Guardamos datos en sesión
+            request.session['nombre'] = usuario.nombre
+            request.session['correo'] = usuario.correo
+            request.session['id_usuario'] = usuario.idUsuario
+            request.session['tipo'] = usuario.tipo  
+
+            return redirect('categorias')
+        except Usuario.DoesNotExist:
+            return render(request, 'login.html', {'error': True})
+    return render(request, 'login.html', {
+            'nombre': request.session.get('nombre'),
+            'tipo': request.session.get('tipo')
+        })
+
+def inicio(request):
+    categorias = Categoria.objects.annotate(total=Count('producto'))
+    correo = request.session.get('correo')
+    return render(request, 'categorias.html', {'categorias': categorias, 'correo': correo})
